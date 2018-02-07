13b6aa3048fbde6107123c3cfca9e679cbd0dce3
Rahul Patni <rpatni@purdue.edu>
2018-02-06 20:38:03 -0500

    creating routes for uploader

--- v1/app.js
+++ v1/app.js
@@ -11,6 +11,35 @@ app.get("/signup", function(req, res) {
     res.render("signup");
 });
 
+app.post("/login", function(req, res) {
+    res.redirect("/home");
+})
+
+app.get("/home", function(req, res) {
+    res.render("home");
+});
+
+app.get("/video/:id", function(req, res) {
+    console.log("video selected: " + req.params.id);
+    var currVideo = {
+        id: req.params.id
+    }
+    res.render("video/new", {video: currVideo});
+});
+
+app.post("/video/:id", function(req, res) {
+    console.log("video upload: " + req.params.id);
+    res.redirect("/video/" + req.params.id +"/upload");
+});
+
+app.get("/video/:id/upload", function(req, res) {
+    console.log("video upload redirect: " + req.params.id);
+    var currVideo = {
+        id: req.params.id
+    }
+    res.render("video/upload", {video: currVideo});
+});
+
 app.get("*", function(req, res) {
     res.send("asdf");
 });
