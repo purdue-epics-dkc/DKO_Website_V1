f63e11c2b0760b35e05319003527db1e4bbe592f
Rahul Patni <rpatni@purdue.edu>
2018-03-30 14:59:33 -0400

    pulling data with tree path

--- mongo_testing/app.js
+++ mongo_testing/app.js
@@ -11,12 +11,17 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
 var videoSchema = new mongoose.Schema({
+    user_id: String, 
+    admin_id: String, 
+    tree_path: String,
     domain: String,
     topic: String, 
     title: String, 
     description: String, 
-    url: String
-});
+    url: String,
+    uploaded: Boolean,
+    approved: Boolean
+}, { collection: 'videos_info' });
 
 var urlToParse = "http://www.khanacademy.org/api/v1/topic/law-of-sines";
 
@@ -35,44 +40,66 @@ app.get("/results", function(req, res)
     });
 });
 
-app.get("/all-results", function(req, res) {
+app.get("/all-results/:domain_id/:video_id", function(req, res) {
     var topic = "trigonometry";
-    createRequestWithTopic(topic);
+    var topic_id = req.params.video_id;
+    if (!(topic_id.length == 0 || topic_id == null)) {
+        topic = topic_id;
+    
+    } 
+
+    console.log("\n\nDOWNLOADING" + topic + "\n\n");
+
+    var domain = "math";
+    var domain_id = req.params.domain_id;
+
+    if (!(domain_id.length == 0 || domain_id == null)) {
+        domain = domain_id;
+    
+    } 
+
+    var path = domain + "+$+";
+
+
+    createRequestWithTopic(topic, path);
     res.send("<h1>Got All Results</h1>")
 });
 
-function createRequestWithTopic(topic) {
+function createRequestWithTopic(topic, path) {
     var topicUrl = "http://www.khanacademy.org/api/v1/topic/" + topic;
 
     request(topicUrl, function(error, response, body) {
         if(!error && response.statusCode == 200){
             var data = JSON.parse(body);
-            goThroughChildren(data.children, topic, "math");
+            goThroughChildren(data.children, topic, "math", path+topic);
         }  
     });
 }
 
-function goThroughChildren(children, topic, domain) {
+function goThroughChildren(children, topic, domain, path) {
     for (var i = 0; i < children.length; i++) {
         var child = children[i];
         console.log(child.kind);
         if (child.kind == "Video") {
-            addChildToDatabase(child, topic, domain)
+            addChildToDatabase(child, topic, domain, path)
         }
         else if (child.kind == "Topic") {
             var currentTopic = child.node_slug;
-            createRequestWithTopic(currentTopic);
+            createRequestWithTopic(currentTopic, path+"+$+");
         }
     }
 }
 
-function addChildToDatabase(child, topic, domain) {
+function addChildToDatabase(child, topic, domain, path) {
     var data = {
+        tree_path: path,
         domain: domain,
         topic: topic, 
         title: child.translated_title, 
         description: child.description, 
-        url: child.url 
+        url: child.url,
+        uploaded: false,
+        approved: false
     };
     // console.log(data);
     Video.create(data, function(err, video) {
